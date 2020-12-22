9b34a9b7ba99a1a647f85e70ab04844d520887e6
DESKTOP-36AQE2I\tomas <tomasz.kaniewski@infofabric.io>
2020-12-22 12:02:43 +0100

    no message

--- main.tf
+++ main.tf
@@ -1,19 +1,27 @@
 variable "someString" {}
 
 # Call Powershell.exe and pass in 'foo' and 'bar'
-data "external" "powershell_test" {
-  program = ["Powershell.exe", "./testScript.ps1"]
+# data "external" "powershell_test" {
+#   program = ["Powershell.exe", "./testScript.ps1"]
 
-  query = {
-    foo = "${var.someString}"
-    bar = "Hardcoded"
+#   query = {
+#     foo = "${var.someString}"
+#     bar = "Hardcoded"
+#   }
+# }
+
+resource "null_resource" "script1" {
+  provisioner "local-exec" {
+    command = "./testScript.ps1"
+
+  interpreter = ["PowerShell", "-File"]
   }
 }
 
-output "firstValue" {
-  value = "${data.external.powershell_test.result.first}"
-}
+# output "firstValue" {
+#   value = "${data.external.powershell_test.result.first}"
+# }
 
-output "secondValue" {
-  value = "${data.external.powershell_test.result.second}"
-}
+# output "secondValue" {
+#   value = "${data.external.powershell_test.result.second}"
+# }
